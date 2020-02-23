74f1777218b2d617cf2d2f29825dce113216d814
KazumichiShirai <acdcacdc1985@hotmail.com>
2020-02-23 18:56:18 +0900

    Copy from https://github.com/KazumichiShirai/reactjs_and_nextjs about firebase

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Sampledata from './fire/SampleData';
+import firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Firebaseの設定
+var config = {
+  apiKey: "AIzaSyArl-TRQxkbaJZADZqM_b2BnLXfZ0TV068",
+  authDomain: "shirai-react.firebaseapp.com",
+  databaseURL: "https://shirai-react.firebaseio.com",
+  projectId: "shirai-react",
+  storageBucket: "shirai-react.appspot.com",
+  messagingSenderId: "186479167192",
+  appId: "1:186479167192:web:3b0c91f31647a04623b9ad",
+  measurementId: "G-NX97G9MBNN"
+};
+
+// Firebaseの初期化
+firebase.initializeApp(config);
+
+
+// Appコンポーネント
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>Fire</h1>
+        <h2>Sample data.</h2>
+        <Sampledata />
+      </div>
+    );
+  }
 }
+
 
 export default App;
